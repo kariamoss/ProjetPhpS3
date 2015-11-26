8c5531bcb4e3d6861ce0baf780b318202be38735
Kariamoss <jehanportable@gmail.com>
2015-11-26 10:16:14 +0100

    Ajout de la vue panier vide

---
+++ application/views/clients/table_panier_vide_v.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jehan
+ * Date: 26/11/2015
+ * Time: 10:05
+ */
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><div class="row">
+    <table>
+        <h1>Votre panier est vide </h1>
+        <tbody>
+            <a  href="<?php echo site_url('Produit_c/displayProduits');?>" class = "button" >
+                Venez voir ce que nous proposons !</a>
+        <tbody>
+    </table>
+</div>
